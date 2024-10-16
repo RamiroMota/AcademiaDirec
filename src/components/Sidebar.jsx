cae14119d195da5d5437f4eabd70678bcd7a613c
Mugirad <mcrades001@gmail.com>
2024-10-16 14:04:36 -0600

    Prueba:Luber

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,7 +27,7 @@ function Sidebar() {
             </ul>
           </li>
           <li>
-            <Link to='/' className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-black transition-colors"> <RiMenuSearchLine className='text-amber-500'/>Investigación</Link>
+            <Link to='/' className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-black transition-colors"> <RiMenuSearchLine className='text-amber-500'/>Invest</Link>
             <ul className='mt-4'>
               <li>
               <Link to="/" className='py-2 px-4 border-l border-gray-400 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-400 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-gray-800'>Proyectos</Link>
