959f61f0390c60712d72f998fe79f5634b6d082f
Mugirad <mcrades001@gmail.com>
2024-10-17 16:47:16 -0600

    Actualizado Sidebar funcionando

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,8 @@ import {
  } from "react-icons/ri";
 
 const Sidebar = () => {
-  const [showSubmenu, setShowSubmenu] = useState(false);
+  const [showSubmenu1, setShowSubmenu1] = useState(false);
+  const [showSubmenu2, setShowSubmenu2] = useState(false);
   return (
     <div className="xh:h-[100vh] fixed xl:static w-full h-full -left-full top-0 bg-gray-800 p-8 flex flex-col justify-between">
       <div>
@@ -20,20 +21,20 @@ const Sidebar = () => {
             <Link to='/' className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-black transition-colors"><RiHome3Line className='text-amber-500'/>Inicio</Link>
           </li>
           <li>
-            <button onClick={() => setShowSubmenu(!showSubmenu)} className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-black transition-colors" type="button">
-              <RiBookOpenLine className='text-amber-500'/>Calidad Educativa<RiArrowDropRightLine className={`w-6 h-6 mt-1 ${showSubmenu && "rotate-90"} transition-all`} />
+            <button onClick={() => setShowSubmenu1(!showSubmenu1)} className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-black transition-colors" type="button">
+              <RiBookOpenLine className='text-amber-500'/>Calidad Educativa<RiArrowDropRightLine className={`w-6 h-6 mt-1 ${showSubmenu1 && "rotate-90"} transition-all`} />
             </button>
-            <ul className={`my-2 ${!showSubmenu && "hidden"}`}>
+            <ul className={`my-2 ${!showSubmenu1 && "hidden"}`}>
               <li>
                   <Link to="/" className='py-2 px-4 border-l border-gray-400 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-amber-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-gray-800 hover:transition-colors hover:text-white'>Secuencia Didáctica</Link>
               </li>
             </ul>
           </li>
           <li>
-            <button className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-black transition-colors hover:text-white" type='button'>
-              <RiMenuSearchLine className='text-amber-500'/>Investigación<RiArrowDropRightLine className={`w-6 h-6 mt-1 ${showSubmenu && "rotate-90"} transition-all`} />
+            <button onClick={() => setShowSubmenu2(!showSubmenu2)} className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-black transition-colors hover:text-white" type='button'>
+              <RiMenuSearchLine className='text-amber-500'/>Investigación<RiArrowDropRightLine className={`w-6 h-6 mt-1 ${showSubmenu2 && "rotate-90"} transition-all`} />
             </button>
-            <ul className={`my-2 ${!showSubmenu && "hidden"}`}>
+            <ul className={`my-2 ${!showSubmenu2 && "hidden"}`}>
               <li>
               <Link to="/" className='py-2 px-4 border-l border-gray-400 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-400 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-gray-800 hover:transition-colors hover:text-white'>Proyectos</Link>
               </li>
