25bc54462ac9e707ea4db03dea8521ebd9c84e41
Ramiro Mota <rockett117@gmail.com>
2024-10-17 16:51:32 -0600

    Se modificaron los elementos del navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-import { RiNotification2Line , RiArrowDownSLine } from 'react-icons/ri'
+import { RiNotification2Line , RiArrowDownSLine, RiVerifiedBadgeFill,RiSpam3Fill, RiAtFill } from 'react-icons/ri'
 
 import { Menu, MenuItem, MenuButton } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
@@ -22,41 +22,29 @@ function Header() {
         transition
         menuClassName="bg-gray-800 P-4"
         >
-          <h1 className='text-slate-300 text-center font-medium'>Notificaciones</h1>
-          <div className='h-[2px] bg-gray-500 w-full'></div>
+          <h1 className='text-slate-300 text-center font-medium mb-4'>Notificaciones</h1>
+          <div className='h-[1px] bg-gray-500 w-full'></div>
             <MenuItem className="p-0 hover:bg-transparent">
-              <Link to="/" className='flex items-center gap-2 py-1 px-2 hover:bg-gray-700 transition-colors rounded-lg'>
-              <img src="./fernando.webp" alt="avatar.webp" className='w-10 h-10 rounded-full object-cover' />
+              <Link to="/" className='flex items-center gap-2 py-1 px-2 hover:bg-gray-700 ease-in duration-500 transition-colors rounded-lg'>
+              <RiVerifiedBadgeFill className='w-8 h-8 text-green-500 bg-green-100 rounded-full' />
                 <div className='text-sm flex flex-col'>
                   <div className='flex items-center justify-between gap-4'>
-                    <span className='text-slate-300 font-medium'>Dr. Fernando Arreola</span>{' '}
+                    <span className='text-slate-300 font-medium'>Fernando Arreola</span>{' '}
                     <span className='text-[12px] text-slate-300'>16/10/2024</span>
                   </div>
-                  <p className='text-slate-500 font-medium'>[Correcciones de Secuencia didáctica]</p>
+                  <p className='text-slate-500 font-medium'>[Tu equipo se aprobó.]</p>
                 </div>
               </Link>
             </MenuItem>
             <MenuItem className="p-0 hover:bg-transparent">
-              <Link to="/" className='flex items-center gap-2 py-1 px-2 hover:bg-gray-700 transition-colors rounded-lg'>
-              <img src="./fernando.webp" alt="avatar.webp" className='w-10 h-10 rounded-full object-cover' />
+              <Link to="/" className='flex items-center gap-2 py-1 px-2 hover:bg-gray-700 ease-in duration-500 transition-colors rounded-lg'>
+              <RiSpam3Fill className='w-8 h-8 text-red-500 bg-red-100 rounded-full' />
                 <div className='text-sm flex flex-col'>
                   <div className='flex items-center justify-between gap-4'>
-                    <span className='text-slate-300 font-medium'>Dr. Fernando Arreola</span>{' '}
+                    <span className='text-slate-300 font-medium'>Fernando Arreola</span>{' '}
                     <span className='text-[12px] text-slate-300'>16/10/2024</span>
                   </div>
-                  <p className='text-slate-500 font-medium'>[Correcciones de Secuencia didáctica]</p>
-                </div>
-              </Link>
-            </MenuItem>
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link to="/" className='flex items-center gap-2 py-1 px-2 hover:bg-gray-700 transition-colors rounded-lg'>
-              <img src="./fernando.webp" alt="avatar.webp" className='w-10 h-10 rounded-full object-cover' />
-                <div className='text-sm flex flex-col'>
-                  <div className='flex items-center justify-between gap-4'>
-                    <span className='text-slate-300 font-medium'>Dr. Fernando Arreola</span>{' '}
-                    <span className='text-[12px] text-slate-300'>16/10/2024</span>
-                  </div>
-                  <p className='text-slate-500 font-medium'>[Correcciones de Secuencia didáctica]</p>
+                  <p className='text-slate-500 font-medium'>[Tu equipo no se aprobó.]</p>
                 </div>
               </Link>
             </MenuItem>
@@ -67,11 +55,21 @@ function Header() {
             <span>Herman Perez López</span>
             <RiArrowDownSLine />
           </MenuButton>}
-          menuClassName="bg-gray-800 text-slate-300  rounded-lg shadow-lg"
+          menuClassName="bg-gray-800 P-4"
           >
-            <MenuItem>Cut</MenuItem>
-            <MenuItem>Copy</MenuItem>
-            <MenuItem>Paste</MenuItem>
+          <h1 className='text-slate-300 text-center font-medium mb-4'><span class="bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-400">Docente</span></h1>
+          <div className='h-[1px] bg-gray-500 w-full'></div>
+            <MenuItem className="p-0 hover:bg-transparent">
+              <Link to="/" className='flex items-center gap-2 py-1 px-2 hover:bg-gray-700 ease-in duration-500 transition-colors rounded-lg'>
+              <RiAtFill className='w-8 h-8 text-orange-500 bg-orange-100 rounded-full' />
+                <div className='text-sm flex flex-col'>
+                  <div className='flex items-center justify-between gap-4'>
+                    <span className='text-slate-300 font-medium'>hernan.perez@upgch.mx</span>
+                  </div>
+                  <p className='text-slate-500 font-medium'>[Dirección de Negocios]</p>
+                </div>
+              </Link>
+            </MenuItem>
         </Menu>
       </nav>
     </header>
