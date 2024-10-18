6b1cad1f2bd83c7ffecf085880a0c3a44a7186db
Mugirad <mcrades001@gmail.com>
2024-10-18 15:11:19 -0600

    Actualizado 404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 function Error404() {
   return (
-    <div>Error404</div>
-  )
+    <div className="flex flex-col items-center justify-center h-screen">   
+
+      <div className="flex flex-col items-center">   
+
+        <img src="https://i.ibb.co/G9DC8S0/404-2.png" alt="404 error" className="mb-8" />
+        <h1 className="text-3xl font-bold mb-2">
+          La sección no existe
+        </h1>
+        <p className="text-white">
+          Lamentamos eso, puedes ir a nuestra sección principal.   
+
+        </p>
+        <a href="/" className="mt-4 bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+          Ir
+        </a>
+      </div>
+      <div>
+        <img src="https://i.ibb.co/ck1SGFJ/Group.png" alt="decorative image" />
+      </div>
+    </div>
+  );
 }
 
-export default Error404
+export default Error404;
